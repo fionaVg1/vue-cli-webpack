90f7260f7fc41a7014107267981e65a150967dce
fionaVg1 <41353276+fionaVg1@users.noreply.github.com>
2020-11-16 11:10:01 +0800

    八皇后组件

---
+++ src/components/TResizeDirective.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="test">
+        <div v-resize:[direction].quiet="onResize">window Height is:{{length}}</div>
+        <input v-model="text"/>
+        <input :value="text" @input="text = $event.target.value"/>
+    </div>
+</template>
+<script>
+import resize from '@/directive/resize.js'
+export default {
+    data(){
+        return{
+            direction:'vertical',
+            length:0,
+            text:'测试'
+        }
+    },
+    directives:{
+        resize
+    },
+    methods:{
+        onResize(length){
+            this.length = length;
+        }
+    }
+}
+</script>
